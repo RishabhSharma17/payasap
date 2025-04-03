740dc23c97fc23d8f8538c25b0fb150e9d046ffc
RishabhSharma17 <rishabhsharma17aug2004@gmail.com>
2025-04-03 19:07:20 +0530

    user origin changed

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,8 +1,6 @@
 import { Appbar } from "../components/Appbar"
 import { Balance } from "../components/Balance";
-import { Users } from "../components/Users";
-
-
+import { Users }  from "./Users";
 
 export const Dashboard = ()=>{
     
