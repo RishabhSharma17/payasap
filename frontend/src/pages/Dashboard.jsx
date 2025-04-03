253cf336aa7bee13bdbc32b35bd36e23930625da
RishabhSharma17 <rishabhsharma17aug2004@gmail.com>
2025-04-03 19:57:19 +0530

    dashboard updated

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,6 +1,6 @@
 import { Appbar } from "../Component/Appbar"
 import { Balance } from "../Component/Balance";
-import { Users }  from "./Component/Users";
+import { Users }  from "../Component/Users";
 
 export const Dashboard = ()=>{
     
